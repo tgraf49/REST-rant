1b22285f72e16b716101756a725144e6adfa1c8c
Tyler Graf <tygraf13@gmail.com>
2023-10-29 05:30:51 -0500

    Adding places view with mock data

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/parrot.jpg" alt="parrot"></img> 
+                    <div>
+                        Photo by <a href="https://unsplash.com/@miklevasilyev">Mikhail Vasilyev</a> on <a href="https://unsplash.com/photos/selective-focus-photography-of-blue-red-and-green-bird-gGC63oug3iY">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>

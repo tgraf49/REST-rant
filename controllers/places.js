62a58764fb8645d748c9f40c51b7606a775821c5
Tyler Graf <tygraf13@gmail.com>
2023-10-29 11:26:59 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const places = require('../models/places.js')
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 });
 
-// router.post('/new'. (req,res) => {
-//     res.render('places/new');
-// });
+router.post('/', (req,res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city){
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state){
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+});
 
 router.get('/', (req, res) =>{
     let places = [{
